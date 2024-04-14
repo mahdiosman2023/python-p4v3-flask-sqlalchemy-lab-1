200c48dd73670194f5651c7a60f2c3571552eca6
mahdi <mahdiosmanu78@gmail.com>
2024-04-15 02:40:19 +0300

    flasksqllab1

--- server/app.py
+++ server/app.py
@@ -21,6 +21,21 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquake/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id== id).first()
+
+    if earthquake:
+        body = { 'id': earthquake.id,
+                'magnitude': earthquake.magnitude,
+                'location': earthquake.location,
+                'year': earthquake.year}
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
 
 
 if __name__ == '__main__':
